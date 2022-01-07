02eeda72dddba7938c1812d94ca3ccbae72e7c2a
Curtis Brooks <curtisbrooks678@gmail.com>
2022-01-07 10:04:47 -0800

    fill out models

---
+++ Factory/Models/Machine.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+
+namespace Factory.Models
+{
+  public class Machine
+  {
+    public int MachineId { get; set; }
+    public string Name { get; set; }
+    public virtual ICollection<EngineerMachine> JoinEntities { get; set; }
+
+    public Engineer()
+    {
+      this.JoinEntities = new HashSet<EngineerMachine>();
+    }
+  }
+}
